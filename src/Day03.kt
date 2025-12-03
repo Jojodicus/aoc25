753d03094575ce35a150eee6f391645386abcdda
jojodicus <jojodicus@gmail.com>
2025-12-03 15:17:43 +0100

    day3

--- src/Day03.kt
+++ src/Day03.kt
@@ -9,9 +9,9 @@ fun main() {
         return "$max$lowerJolt"
     }
 
-    fun part1(input: List<String>): Long = input.sumOf { jolt(it, 2).toLong() }
+    fun part1(input: List<String>) = input.sumOf { jolt(it, 2).toLong() }
 
-    fun part2(input: List<String>): Long = input.sumOf { jolt(it, 12).toLong() }
+    fun part2(input: List<String>) = input.sumOf { jolt(it, 12).toLong() }
 
     val input = readInput("Day03")
     printSolution { part1(input) }
