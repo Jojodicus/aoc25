a9cff16d1757a25c5b8727b7ff7b6be717edd3e7
jojodicus <jojodicus@gmail.com>
2025-12-09 09:46:57 +0100

    day 9 part 2

--- src/Day09.kt
+++ src/Day09.kt
@@ -1,3 +1,4 @@
+import java.util.concurrent.LinkedBlockingQueue
 import kotlin.math.abs
 import kotlin.math.max
 import kotlin.math.min
@@ -24,11 +25,73 @@ fun main() {
     }
 
     fun part2(input: List<String>): Long {
-        return 0L
-    }
+        val tiles = input.map {
+            val (x, y) = it.split(",").map(String::toLong)
+            CommunistTile(x, y)
+        }
 
-    printSolution { part1(readInput("Day09-test")) }
-    printSolution { part2(readInput("Day09-test")) }
+        val xMap = tiles.asSequence().map { it.x }.distinct().sorted().withIndex().associate { (i, x) -> x to i }
+        val yMap = tiles.asSequence().map { it.y }.distinct().sorted().withIndex().associate { (i, y) -> y to i }
+
+        val grid = MutableList(yMap.size) {
+            MutableList(xMap.size) { '.' }
+        }
+
+        // green
+        for ((tile0, tile1) in (tiles + tiles.first()).zip(listOf(tiles.first()) + tiles)) {
+            val tile0y = yMap[tile0.y]!!
+            val tile0x = xMap[tile0.x]!!
+            val tile1y = yMap[tile1.y]!!
+            val tile1x = xMap[tile1.x]!!
+            for (y in min(tile0y, tile1y)..max(tile0y, tile1y)) {
+                for (x in min(tile0x, tile1x)..max(tile0x, tile1x)) {
+                    grid[y][x] = 'X'
+                }
+            }
+        }
+
+        // red
+        for (tile in tiles) {
+            grid[yMap[tile.y]!!][xMap[tile.x]!!] = '#'
+        }
+
+        // flood
+        val startX = grid.first().withIndex().find { it.value == 'X' }?.index ?: throw IllegalStateException()
+        val queue = LinkedBlockingQueue(listOf(CommunistTile(startX.toLong(), 1)))
+        while (queue.isNotEmpty()) {
+            val tile = queue.poll()
+
+            if (grid[tile.y.toInt()][tile.x.toInt()] != '.') continue
+            grid[tile.y.toInt()][tile.x.toInt()] = 'X'
+
+            for (next in listOf(
+                CommunistTile(tile.x - 1, tile.y), CommunistTile(tile.x + 1, tile.y),
+                CommunistTile(tile.x, tile.y - 1), CommunistTile(tile.x, tile.y + 1)
+            )) {
+                queue.offer(next)
+            }
+        }
+
+        // optional: print
+//        grid.forEach { println(it.joinToString("")) }
+
+        var maxArea = 0L
+
+        tiles.forEach { tile1 ->
+            tiles.forEach inner@{ tile2 ->
+                for (y in min(yMap[tile1.y]!!, yMap[tile2.y]!!)..max(yMap[tile1.y]!!, yMap[tile2.y]!!)) {
+                    for (x in min(xMap[tile1.x]!!, xMap[tile2.x]!!)..max(xMap[tile1.x]!!, xMap[tile2.x]!!)) {
+                        if (grid[y][x] == '.') return@inner
+                    }
+                }
+
+                val area = (abs(tile1.x - tile2.x) + 1) * (abs(tile1.y - tile2.y) + 1)
+                maxArea = max(maxArea, area)
+            }
+        }
+
+        return maxArea
+    }
 
     val input = readInput("Day09")
     printSolution { part1(input) }
